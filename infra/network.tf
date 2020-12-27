124c67dc791185e8bdfd16eea9d03fee17d1135a
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2020-12-27 11:13:41 +0900

    loadbalancer http

--- infra/network.tf
+++ infra/network.tf
@@ -115,9 +115,3 @@ resource "aws_nat_gateway" "nat_gateway_1" {
   subnet_id = aws_subnet.public_1.id
   depends_on = [aws_internet_gateway.practice]
 }
-
-resource "aws_route" "private" {
-  route_table_id = aws_route_table.private.id
-  nat_gateway_id = aws_nat_gateway.practice.id
-  destination_cidr_block = "0.0.0.0/0"
-}
--- infra/s3.tf
+++ infra/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "alb_log" {
-  bucket = "alb-log-pragmatic-terraform"
+  bucket = "alb-log-practice-docker-terraform"
 
   lifecycle_rule {
     enabled = true
