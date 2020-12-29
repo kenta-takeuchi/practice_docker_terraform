182c65d1c80f216b6511399a2cdd95d49ca0d9f2
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2020-12-29 15:37:33 +0900

    buildspec

--- push_to_ecr.sh
+++ push_to_ecr.sh
@@ -2,7 +2,6 @@ echo Build and push started on `date`
 echo Building the Docker image...
 
 REPO=$(aws ecr describe-repositories --repository-names practice --output text --query "repositories[0].repositoryUri")
-IMAGE=$REPO
 
 for repo in api; do
   docker tag "${repo}:latest" "${REPO}/${repo}:latest"
@@ -12,8 +11,4 @@ done
 echo Push completed on `date`
 
 echo Writing image definitions file...
-cat <<__JSON__ > imagedefinitions.json
-[
-  {"name": "api", "imageUri": "${REPO}/api:latest"},
-]
-__JSON__
+printf '[{"name":"api","imageUri":"%s"}]' "${REPO}/api}:latest" > imagedefinitions.json
