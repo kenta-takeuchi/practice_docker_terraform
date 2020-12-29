0c3a60fe05a795f26f064eab418e49811f048076
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2020-12-29 21:57:24 +0900

    codebuild buildspecの変更

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -26,4 +26,59 @@ resource "aws_ecr_lifecycle_policy" "practice" {
 EOF
 }
 
+resource "aws_ecr_repository" "client" {
+  name = "client"
+}
+
+resource "aws_ecr_lifecycle_policy" "client" {
+  repository = aws_ecr_repository.client.name
+
+  policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 10 release tagged images",
+        "selection": {
+          "tagStatus": "tagged",
+          "tagPrefixList": ["client"],
+          "countType": "imageCountMoreThan",
+          "countNumber": 10
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+EOF
+}
+
+resource "aws_ecr_repository" "api" {
+  name = "api"
+}
+
+resource "aws_ecr_lifecycle_policy" "api" {
+  repository = aws_ecr_repository.api.name
+
+  policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 10 release tagged images",
+        "selection": {
+          "tagStatus": "tagged",
+          "tagPrefixList": ["api"],
+          "countType": "imageCountMoreThan",
+          "countNumber": 10
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+EOF
+}
 
--- push_to_ecr.sh
+++ push_to_ecr.sh
@@ -1,14 +1,16 @@
-echo Build and push started on `date`
-echo Building the Docker image...
+echo Push the Docker image...
 
-REPO=$(aws ecr describe-repositories --repository-names practice --output text --query "repositories[0].repositoryUri")
+echo Logging in to Amazon ECR...
+$(aws ecr get-login --region ${AWS_REGION} --no-include-email)
+
+ECR_REPO_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 
 for repo in api; do
-  docker tag "${repo}:latest" "${REPO}/${repo}:latest"
-  docker push "${REPO}/${repo}:latest"
+  docker tag "${repo}:latest" "${ECR_REPO_URI}/${repo}:latest"
+  docker push "${ECR_REPO_URI}/${repo}:latest"
 done
 
 echo Push completed on `date`
 
 echo Writing image definitions file...
-printf '[{"name":"api","imageUri":"%s"}]' "${REPO}/api:latest" > imagedefinitions.json
+printf '[{"name":"api","imageUri":"%s"}]' "${ECR_REPO_URI}/api:latest" > imagedefinitions.json
